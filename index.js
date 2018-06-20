64051c6af5af3018f6904a02b1084ced6de5f325
MaxMEllon <maxmellon1994@gmail.com>
2018-06-20 22:12:49 +0900

    :sparkles: Legacy style

--- index.js
+++ index.js
@@ -3,14 +3,18 @@
  * @param token {string} - jwt token
  * @return payload {Object}
  */
-export default function parser(token) {
+function parser(token) {
   try {
-    const base64Url = token.split('.')[1]
-    const base64 = base64Url.replace('-', '+').replace('_', '/')
+    var base64Url = token.split('.')[1]
+    var base64 = base64Url.replace('-', '+').replace('_', '/')
     if (window && window.atob) return JSON.parse(window.atob(base64))
-    const atob = (str) => Buffer.from(str, 'base64').toString('binary')
+    var atob = function (str) {
+      return Buffer.from(str, 'base64').toString('binary')
+    }
     return JSON.parse(atob(base64))
   } catch (_err) {
     throw new Error('isomorphic-jwt-parser: %o', err)
   }
 }
+
+module.exports = parser
